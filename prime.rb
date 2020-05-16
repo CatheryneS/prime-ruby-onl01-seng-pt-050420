72191ee71c701d61a5be2d94cbc814b88eb9c0db
Catheryne Small <catheryn.small@gmail.com>
2020-05-16 06:28:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,6 @@
 def prime?(number)
-  n = 2
-  while n < number
-    
-    return false if number % n == 0
-    n += 1
+(2..(num - 1)).each do |n|
+    return false if num % n == 0
   end
   true
 end
