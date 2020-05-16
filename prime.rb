01ed9493db0ec0fa4bdff754542ff13e19487ed2
Catheryne Small <catheryn.small@gmail.com>
2020-05-16 06:31:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,9 @@ def prime?(number)
     n = 2
   while n < number
     if number % n == 0
-      return false 
-    elsif number <= 0
+      return false
+    end
+    if number <= 0
       return false
     n += 1
   end
